b86c8f583d4ed77fd5d3d7e9f05bd182df3addda
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-23 15:26:25 +0530

    Added cards

---
+++ src/components/cards.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+import { styles } from './styles/cards.styles';
+
+const Cards = () => {
+
+  return (
+    <View>
+        
+    </View>
+  );
+};
+
+export default Cards
